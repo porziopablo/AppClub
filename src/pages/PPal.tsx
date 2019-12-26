a3e863d28ec1b827a5c8fec0f5fef5aa60273d5a
Pablo Porzio <porziopablo@outlook.com>
2019-12-26 15:55:19 -0300

    Prueba 26-12-19

---
+++ src/pages/PPal.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {IonContent, IonCard } from '@ionic/react';
+
+const PPal: React.FC = () => {
+    return (
+      <IonContent>
+            <IonCard className="logo-card">
+                <img src="/assets/logo.png" alt="logo" />
+            </IonCard>
+
+      </IonContent>  
+
+  );
+};
+
+
+export default PPal;
