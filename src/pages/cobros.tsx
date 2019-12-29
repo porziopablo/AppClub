018e2e8bbb06c4a64db1dbf74da47e2911a4b846
Pablo Porzio <porziopablo@outlook.com>
2019-12-29 16:47:49 -0300

    Vista Cobros - 29-12-19

--- src/pages/cobros.tsx
+++ src/pages/cobros.tsx
@@ -1,13 +1,55 @@
 import React from 'react';
-import { IonHeader, IonPage} from '@ionic/react';
+import {IonPage, IonContent, IonItem, IonLabel, IonInput, IonItemGroup, IonButton, IonIcon, IonText, IonRow, IonCol, IonGrid} from '@ionic/react';
+import { checkmarkCircle } from 'ionicons/icons';
 
 const Cobros: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-      </IonHeader>
+          <IonContent>
+              <IonItemGroup>
+                  <IonItem>
+                      <IonLabel>Cobros</IonLabel>
+                  </IonItem>
+                  <IonItem>
+                      <IonInput placeholder="DNI Jugador/a" inputMode="text" minlength={1} required={true}></IonInput>
+                  </IonItem>
+                  <IonItem>
+                      <IonInput placeholder="Monto" inputMode="numeric" min="0" required={true} type="number"></IonInput>
+                      <IonButton fill="outline" slot="end" size="default" color="success">
+                          <IonIcon icon={checkmarkCircle} />
+                      </IonButton>
+                  </IonItem>
+              </IonItemGroup>
+              <IonItemGroup>
+                  <IonItem>
+                      <IonLabel>Balance</IonLabel>
+                  </IonItem>
+                  <IonItem>
+                      <IonText>
+                          <h4>$1500,0</h4>
+                      </IonText>
+                      <IonButton  fill = "outline" slot="end" size="default" color="success">
+                          <IonIcon icon={checkmarkCircle} />
+                      </IonButton>
+                  </IonItem>
+                  <IonGrid>
+                      <IonRow align-content-center>
+                          <IonCol>
+                              <IonButton fill = "outline">
+                                  Historial de Cobros
+                              </IonButton>
+                          </IonCol>  
+                      </IonRow>
+                  </IonGrid>  
+              </IonItemGroup>
+          </IonContent>  
     </IonPage>
   );
 };
 
-export default Cobros;
+export default Cobros; 
+
+/*
+ - Usamos teclado específico para inputs numericos?
+ - Falta Boton Comprobante
+ */
