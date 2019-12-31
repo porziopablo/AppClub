649d9ebecf7abbca242a834b4d06c2a5ebfd5af7
ivan aprea <ivanaprea.cabj@gmail.com>
2019-12-31 13:28:33 -0300

    Agrega vista logIn

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -1,11 +1,40 @@
-﻿import React from 'react';
-import { IonHeader, IonPage } from '@ionic/react';
+﻿import React, { useState } from 'react';
+import { IonPage, IonContent, IonLabel, IonInput, IonItem, IonText, IonCheckbox, IonButton, IonModal  } from '@ionic/react';
+import logoClub from '../images/logoclub.jpg'
+import '../theme/logIn.css';
+
 
 const LogIn: React.FC = () => {
+
+    const [showPass, setShowPass] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+
     return (
         <IonPage>
-            <IonHeader>
-            </IonHeader>
+            <IonContent>
+                <h1>
+                    <img id='logoClub' src={logoClub} alt="Logo del club" />
+                </h1>
+                <IonItem id='div1'>
+                    <IonLabel position="floating"><IonText class='label2'>Usuario</IonText></IonLabel>
+                    <IonInput required type="text" ></IonInput>
+                </IonItem>
+                <IonItem>
+                    <IonLabel position="floating"><IonText class='label2'>Contraseña</IonText></IonLabel>
+                    <IonInput required type={(showPass===true) ? 'text' : 'password'}></IonInput>
+                </IonItem>
+                <div id='verPas'>
+                    <IonLabel >Mostrar contraseña</IonLabel>
+                    <IonCheckbox class='CB' onClick={() => setShowPass(!showPass)}></IonCheckbox>
+                </div>
+                <IonButton id='botModal'>Iniciar Sesion</IonButton>
+                <IonModal isOpen={showModal}>
+                    <p>This is modal content</p>
+                    <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+                </IonModal>
+                <IonButton id='botModal' onClick={() => setShowModal(true)}>Registrarse</IonButton>
+            </IonContent>
         </IonPage>
     );
 };
